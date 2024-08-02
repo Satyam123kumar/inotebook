d99ce11cda27669a8c2079afdb0c71257f415974
Satyam kumar <58924096+Satyam123kumar@users.noreply.github.com>
2024-08-02 22:21:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(cors({
-  origin: "https://todo-taupe-omega.vercel.app"
+  origin: "https://inotebook-chi-blush.vercel.app/login"
 }));
 //Available Routes
 
