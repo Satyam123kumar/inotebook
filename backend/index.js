75b5246ee985d34ade68ce2acfef2a938f51595a
Satyam kumar <58924096+Satyam123kumar@users.noreply.github.com>
2024-08-02 22:18:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,19 @@ const app = express()
 const PORT = 5000;
 app.use(cors())
 app.use(express.json())
+
+app.use(cors({
+  origin: "https://todo-taupe-omega.vercel.app"
+}));
 //Available Routes
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+// app.get("/", (req, res) => {
+//     app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// });
 
 app.listen(PORT, () => {
     console.log("server started")
